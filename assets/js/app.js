f66342afd0172bf192046f04740dd0f29a9705e1
K4gur0 <kenshin91cb@gmail.com>
2020-02-11 16:49:10 +0100

    MAJ front presentation nomade

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,14 +12,16 @@ import '../css/app.scss';
 const fondEcran = require('../images/apart1.jpg');
 const logo_blanc = require('../images/qleaq_blanc.png');
 const logo_bleu = require('../images/qleaq_bleu.png');
-const formulaire = require('../images/svg/formulaire.svg');
-const formulaire_proprio = require('../images/svg/formulaire_proprio.svg');
+const login_nomade = require('../images/svg/login_nomade.svg');
+const login_proprio = require('../images/svg/login_proprio.svg');
+const registration_nomade = require('../images/svg/registration_nomade.svg');
+const registration_proprio = require('../images/svg/registration_proprio.svg');
 const locataire = require('../images/svg/locataire.svg');
-const proprio_sub = require('../images/svg/proprio_sub.svg');
 const vision = require('../images/svg/vision.svg');
 const best_place = require('../images/svg/best_place.svg');
 const mission = require('../images/svg/mission.svg');
 const solution = require('../images/svg/solution.svg');
+const offres = require('../images/svg/offres.svg')
 
 
 var html = "<img src='${fondEcran}'>";
